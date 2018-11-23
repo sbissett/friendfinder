20a783cb96c07cb94842072efdcf4fb40266f8f9
Stephen Bissett <steve.bissett5@gmail.com>
2018-11-23 12:25:56 -0700

    withfriends

---
+++ app/data/friends.js
@@ -0,0 +1,69 @@
+// DATA
+var friends = [
+    {
+       "name":"Bugs Bunny",
+       "photo":"../../public/assets/images/Bugs-Bunny.jpeg",
+       "scores":[
+          "5",
+          "1",
+          "4",
+          "4",
+          "5",
+          "1",
+          "2",
+          "5",
+          "4",
+          "1"
+       ]
+    }, {
+        "name":"Lola \-Bunny",
+        "photo":"../../public/assets/images/Lola-Bunny.jpeg",
+        "scores":[
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+    }, {
+        "name":"Daffy Duck",
+        "photo":"../../public/assets/images/Daffy-Duck.jpeg",
+        "scores":[vv
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+      ]
+    }, {
+        "name":"Tina Duck",
+        "photo":"../../public/assets/images/Tina-Duck.jpeg",
+        "scores":[
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+      ]
+    } 
+];
+    
+    
+    
+// Here we export the array. This makes it accessible to other files using require. 
+module.exports = friends;
