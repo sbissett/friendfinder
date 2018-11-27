ede196ae736db70bb8ff879f1205845a6b22743c
Stephen Bissett <steve.bissett5@gmail.com>
2018-11-27 10:55:06 -0700

    withupdates

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 // Dependencies
 // =============================================================
 var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
+
 
 // Sets up the Express App
 // =============================================================
@@ -10,14 +9,13 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing. You tell express to parse data
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.text());
-app.use(bodyParser.json({type:'application/vnd.api+json'}));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+//app.use(express.json({type:'application/vnd.api+json'}));
 
 
 //Tried to add css files to server
-// app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(('public')));
 
 
 //=====================================================================
